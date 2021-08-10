fe58f4546e9fe770e3af032b01f7dfe26aae455e
mabenan <doener123@googlemail.com>
2021-08-10 16:42:24 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { Application } from "express";
+export class ServerConfig {
+    PARSE_APPNAME: string;
+    PARSE_DATABASEURI: string;
+    PARSE_APPID: string;
+    PARSE_MASTERKEY: string;
+    PARSE_SERVERURL: string;
+    PARSE_PUBLICSERVERURL: string;
+    ROUTE: string;
+    PORT: number;
+    LOCALPORT: number;
+    MAIN_DATABASE: string;
+  }
+
+class Instance {
+ PARSE_INSTANCE : Application; 
+ PARSE_DASHBOARD : Application;
+ APP: Application;
+ CONFIG : ServerConfig;
+    initialExpressStackSize: number;
+ LOGGER : any = require("parse-server/lib/logger");
+}
+
+export const instance = new Instance();
